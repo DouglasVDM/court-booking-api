5a89c7375cdf84e7795ce04d39d79d663650ebfd
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-03 13:41:28 +0200

    Get days of the week

--- index.js
+++ index.js
@@ -90,6 +90,19 @@ app.get("/bookings", async (req, res) => {
   }
 });
 
+app.get("/days-of-week", async (req, res) => {
+  try {
+    const query =
+      "SELECT * FROM days_of_week";
+    const { rows } = await pool.query(query);
+    res.status(200).json(rows);
+    console.log("Success: Get all days of week");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Failed! Server error, could not get days of week");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
